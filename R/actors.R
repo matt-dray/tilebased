9e8df85fee5aa1292ea1818729a3b1b6c114478a
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2024-06-28 21:30:56 +0100

    Add enemy sprite and movement, add place_tiles function

--- R/actors.R
+++ R/actors.R
@@ -6,7 +6,6 @@
 .move_player <- function(room, kp = c("Up", "Down", "Left", "Right")) {
 
   player_loc <- which(room == "@")
-  wrap_tiles <- .find_wrap_tiles(room)
   room_y_max <- nrow(room)
   room[player_loc] <- "."
 
@@ -24,22 +23,106 @@
 
 }
 
-#' Find Tiles in Player's Row and Column that will Wrap if Exceeded
+#' Move Enemy Around Room
 #' @param room Matrix.
-#' @return Numeric vector.
+#' @param dist Matrix.
+#' @return Matrix.
 #' @noRd
-.find_wrap_tiles <- function(room) {
-
-  room_n <- matrix(seq(length(room)), nrow = nrow(room), ncol = ncol(room))
-  loc <- which(room == "@", arr.ind = TRUE)
-  col_vals <- room_n[, loc[, "col"]]
-  row_vals <- room_n[loc[, "row"], ]
-
-  c(
-    up = min(col_vals),
-    down = max(col_vals),
-    left = min(row_vals),
-    right = max(row_vals)
+.move_enemy <- function(room, dist) {
+  
+  en_loc <- which(room == "E")
+  player_loc <- which(room == "@")
+  n_rows <- nrow(room)
+  
+  ind <- c(
+    n = en_loc - 1,
+    s = en_loc + 1,
+    e = en_loc + n_rows,
+    w = en_loc - n_rows
   )
+  
+  tiles <- c(
+    n = room[ind["n"]],
+    s = room[ind["s"]],
+    e = room[ind["e"]],
+    w = room[ind["w"]]
+  )
+  
+  dist <- c(
+    n = if (tiles["n"] %in% c(".", "@")) dist[ind["n"]],
+    s = if (tiles["s"] %in% c(".", "@")) dist[ind["s"]],
+    e = if (tiles["e"] %in% c(".", "@")) dist[ind["e"]],
+    w = if (tiles["w"] %in% c(".", "@")) dist[ind["w"]]
+  )
+  
+  
+  direction <- sample(names(dist[dist == min(dist)]), 1)
+  en_loc_new <- ind[names(ind) == direction]
+  
+  room[en_loc] <- "."
+  room[en_loc_new] <- "E"
+  
+  room
+  
+}
 
+#' Create a Mesh of Distances to the Player
+#' @param room Matrix.
+#' @return Matrix.
+#' @noRd
+.get_distance_map <- function(room) {
+  
+  dist <- .initiate_distance_map(room)
+  
+  start <- which(room == "@")
+  frontier <- start
+  visited <- c()
+  
+  while (length(frontier) > 0) {
+    
+    current  <- frontier[1]  # set first tile of frontier as current
+    frontier <- frontier[!frontier == current]  # remove current tile from frontier
+    visited  <- append(visited, current)  # mark current as visited
+    
+    neighbours <- .get_neighbours(room, current)  # get vector of neighbour indices
+    neighbours <- neighbours[!neighbours %in% visited]
+    
+    for (neighbour in neighbours) {
+      if (!neighbour %in% visited) {  # only assign distance to unvisited neighbours
+        dist[neighbour] <- dist[current] + 1  # assign distance, one more than parent
+      }
+    }
+    
+    frontier <- append(frontier, neighbours)  # add neighbour to the frontier
+    
+  }
+  
+  dist
+  
+}
+
+#' Create a Basic Mesh of Distances for the Room Layout
+#' @param room Matrix.
+#' @return Matrix.
+#' @noRd
+.initiate_distance_map <- function(room) {
+  
+  dist <- room
+  dist[which(dist != "#")] <- 0
+  dist[which(dist == "#")] <- Inf
+  matrix(as.numeric(dist), nrow(dist), ncol(dist))
+  
+}
+
+.get_neighbours <- function(room, current) {
+  
+  n_rows <- nrow(room)
+  
+  c(
+    if (room[current - n_rows] != "#") { current - n_rows },
+    if (room[current - 1] != "#") { current - 1 },
+    if (room[current + 1] != "#") { current + 1 },
+    if (room[current + n_rows] != "#") { current + n_rows }
+  )
+  
 }
